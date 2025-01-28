60f6190249e952d2fe8cf7eb2d4d59967ebaad32
dr3gos - macbook <for.my.games.email@gmail.com>
2025-01-28 16:01:04 +0200

    add files

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       dragos                                                       #
+# 	Created:      1/28/2025, 3:59:01 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
